339da0e1ffb62e0196584e382a10671c4c22cbc4
dugasmate <dugasmate@gmail.com>
2018-07-18 17:29:47 +0200

    add interface for repositories

---
+++ gfX/Interfaces/ICrudRepositories.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace gfX.Interfaces
+{
+    public interface ICrudRepositories<T> where T : class
+    {
+        Task Create(T entity);
+        Task<T> SelectById(ObjectId id);
+        Task<List<T>> SelectAll();
+        Task<List<T>> FilterByField(string fieldName, string fieldValue);
+        Task Update(ObjectId id, string updateFieldName, string updateFieldValue);
+        Task Delete(ObjectId id);
+    }
+}
