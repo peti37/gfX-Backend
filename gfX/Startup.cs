a1cd556cb818d5659778ab8d439d5c37f82eae82
dugasmate <dugasmate@gmail.com>
2018-07-19 15:26:04 +0200

    enable cors

--- gfX/Startup.cs
+++ gfX/Startup.cs
@@ -16,12 +16,18 @@ namespace gfX
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +43,11 @@ namespace gfX
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseCors("MyPolicy");
+
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
