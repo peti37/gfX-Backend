f372e698b06b86b82a27037ddf1985a576bc51db
dugasmate <dugasmate@gmail.com>
2018-07-24 15:57:12 +0200

    add testing environment separated and userservice

--- gfX/Program.cs
+++ gfX/Program.cs
@@ -20,6 +20,7 @@ namespace gfX
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:63047")
                 .Build();
     }
 }
--- gfX/Startup.cs
+++ gfX/Startup.cs
@@ -19,6 +19,7 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using gfX.Entities;
+using gfX.Services;
 
 namespace gfX
 {
@@ -84,6 +85,7 @@ namespace gfX
                     };
                 });
             services.AddScoped<ICrudRepositories<GFXUser>, GFXUserRepository>();
+            services.AddScoped<GFXUserService>();
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
                 builder.AllowAnyOrigin()
@@ -91,7 +93,65 @@ namespace gfX
                        .AllowAnyHeader();
             }));
         }
-        
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.Configure<AppSettings>(Configuration.GetSection("TestConnection"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+
+            })
+                .AddCookie()
+                .AddOAuth("GitHub", options =>
+                {
+                    options.ClientId = Configuration["GitHub2:ClientId"];
+                    options.ClientSecret = Configuration["GitHub2:ClientSecret"];
+                    options.CallbackPath = new PathString("/signin-github");
+                    options.Scope.Add("read:org");
+
+                    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                    options.UserInformationEndpoint = "https://api.github.com/user";
+
+                    options.SaveTokens = true;
+
+                    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                    options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                    options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+                    options.ClaimActions.MapJsonKey("urn:github:email", "email");
+
+                    options.Events = new OAuthEvents
+                    {
+                        OnCreatingTicket = async context =>
+                        {
+                            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                            response.EnsureSuccessStatusCode();
+
+                            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                            context.RunClaimActions(user);
+                        }
+                    };
+                });
+            services.AddScoped<ICrudRepositories<GFXUser>, GFXUserRepository>();
+            services.AddScoped<GFXUserService>();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
